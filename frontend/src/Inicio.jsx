742f3a6967c3f8ebc75a4cd24025e296b5207bf2
FacuStoffel <stoffelfacu29@gmail.com>
2023-11-02 22:35:08 -0300

    mejoras en cards

--- frontend/src/Inicio.jsx
+++ frontend/src/Inicio.jsx
@@ -12,45 +12,59 @@ export default function Inicio() {
         <p id="p2">Si no encontras uno que te guste podes pedir el tuyo personalizado</p>
       </div>
       <div className='container' id="cards_inicio">
+        
         <div className="card" >
-          <img src="./assets/img/luffy_g5.jpg" className="card-img-top" alt="..." />
+          <img src="./assets/img/luffy_g5.jpg" className="card-img-top" alt="luffy_g5" />
           <div className="card-body">
             <h5 className="card-title">Luffy Gear 5</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="card-text"><span className="negrita">Tamaño:</span> A5</p>
+            <p className="card-text"><span className="negrita">Anime:</span> One Piece</p>
+            <p className="card-text precio"><span className="negrita">Precio:</span> $$$$$</p>
             {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
           </div>
         </div>
+
         <div className="card" >
-          <img src="./assets/img/gojo.jpg" className="card-img-top" alt="..." />
+          <img src="./assets/img/gojo.jpg" className="card-img-top" alt="gojo" />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 className="card-title">Satoru Gojo</h5>
+            <p className="card-text"><span className="negrita">Tamaño:</span> A4</p>
+            <p className="card-text"><span className="negrita">Anime:</span> Jujutsu Kaisen</p>
+            <p className="card-text precio"><span className="negrita">Precio:</span> $$$$$</p>
             {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
           </div>
         </div>
+
         <div className="card" >
-          <img src="./assets/img/toji.jpg" className="card-img-top" alt="..." />
+          <img src="./assets/img/toji.jpg" className="card-img-top" alt="toji" />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 className="card-title">Toji Fushiguro</h5>
+            <p className="card-text"><span className="negrita">Tamaño:</span> A4</p>
+            <p className="card-text"><span className="negrita">Anime:</span> Jujutsu Kaisen</p>
+            <p className="card-text precio"><span className="negrita">Precio:</span> $$$$$</p>
             {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
           </div>
         </div>
+
         <div className="card" >
-          <img src="./assets/img/eva01.jpg" className="card-img-top" alt="..." />
+          <img src="./assets/img/eva01.jpg" className="card-img-top" alt="eva01" />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 className="card-title">Eva 01</h5>
+            <p className="card-text"><span className="negrita">Tamaño:</span> A4</p>
+            <p className="card-text"><span className="negrita">Anime:</span> Evangelion</p>
+            <p className="card-text precio"><span className="negrita">Precio:</span> $$$$$</p>
             {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
           </div>
         </div>
 
       </div>
 
-
-      <span class="material-symbols-outlined">
-        local_shipping
-      </span>
+      <div className='envio'>
+        <span className="material-symbols-outlined">
+          local_shipping
+        </span>
+        <p>Envios a todo el pais</p>
+      </div>
     </>
   )
 }
